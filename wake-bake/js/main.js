f0a005d6aa187dd4df012b1ef7f84223dbcabd82
Gloshkina <christinaglonina@gmail.com>
2025-10-08 16:44:08 +0400

    Homework__151

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -108,7 +108,7 @@
 
     // Галлерея-слайдер 
 
-    const swiper = new Swiper('.gallery__slider', {
+    new Swiper('.gallery__slider', {
         spaceBetween: 15,
         slidesPerView: 1.5,
 
@@ -136,6 +136,33 @@
 
     });
 
+    // Галлерея-слайдер 
+
+    new Swiper('.testimonials__slider', {
+        spaceBetween: 0,
+        slidesPerView: 1,
+        centeredSlides: true,
+
+        navigation: {
+            nextEl: '.testimonials__next',
+            prevEl: '.testimonials__prev',
+        },
+
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+        },
+
+        breakpoints: {
+            901: {
+                slidesPerView: 1.5,
+            },
+            1201: {
+                slidesPerView: 2.1,
+            },
+        }
+
+    });
 
 
 })()
