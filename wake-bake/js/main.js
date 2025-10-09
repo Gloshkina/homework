b0e43936debf6ff31061ee4ab2e22dc71aebc666
Gloshkina <christinaglonina@gmail.com>
2025-10-09 22:33:06 +0400

    Homework__155

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -164,5 +164,10 @@
 
     });
 
+    // Маска для телефона
+
+    const telInputs = document.querySelectorAll('input[type="tel"]')
+    const im = new Inputmask('+7(999) 999-99-99')
+    im.mask(telInputs)
 
 })()
